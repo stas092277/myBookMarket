29d47b34707cf10ee9b3a1daa2ac7b3d4a9b80b4
Станислав Чебыкин <chebykin-s@corp.ingos.ru>
2019-10-03 19:32:08 +0300

    first worl sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavMenuComponent } from './sidenav-menu/sidenav-menu.component';
+import { BookListComponent } from './sidenav-menu/category-book-list/book-list/book-list.component';
+import { CategoryBookListComponent } from './sidenav-menu/category-book-list/category-book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavMenuComponent
+    SidenavMenuComponent,
+    BookListComponent,
+    CategoryBookListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sidenav-menu/sidenav-menu.component.ts
+++ src/app/sidenav-menu/sidenav-menu.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-sidenav-menu',
   templateUrl: './sidenav-menu.component.html',
   styleUrls: ['./sidenav-menu.component.css']
 })
-export class SidenavMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SidenavMenuComponent{
+  showFiller = false;
 }
+
+
